fce46aa620b4733db4c40eb80cd525fc0d49aefa
Nikita <kit.zaver@yandex.ru>
2022-11-13 16:57:36 +0300

    Add Home Work All

---
+++ work36/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+
+int[] InitArray()
+{
+    Random rnd = new Random();
+    int k = rnd.Next(3,7);
+    int[] result = new int[k];
+    
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(1,100);
+    }
+
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int acum = 0;
+void CheckingAnArrayForParity(int[] array)
+{
+    
+    for (int i = 1; i < array.Length; i = i+2)
+    {        
+        acum =  acum +array[i];
+    }
+    Console.Write($"{acum} ");
+}
+
+int[] a = InitArray();
+// int[] b = {3, 7, 23, 12};
+PrintArray(a);
+CheckingAnArrayForParity(a);
---
+++ work38/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+//  Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+double[] InitArray(int n) 
+{
+    double[] array = new double[n];
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++) 
+    {
+    array[i] = rand.Next(1, 100) + rand.NextDouble();
+    array[i] = Math.Round(array[i], 2);
+    Console.Write($"{array[i]} ");
+    } 
+
+    return array;
+}
+
+(double,double) GetMaxAndMinSumm(double[] array)
+{
+    double max = 0; 
+    double min = 100;
+    for (int i = 0; i < array.Length -1; i++)
+    {   
+        if(array[i]>max) max = array[i];
+        else if (array[i]<min) min = array[i];
+    }
+    return (max, min);
+}
+
+double[] a = InitArray(5);
+(double max, double min) = GetMaxAndMinSumm(a);
+Console.WriteLine();
+Console.WriteLine($"Разницу между максимальным и минимальным : {Math.Round(max - min, 2)} ");
